bb379775c313460f82c832c1b0e92b27a51c94ad
Bodula-sravani <sravanibodula@gmail.com>
2023-06-27 12:21:22 +0530

    [ADD] user login api

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+
+namespace XploreIN.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public LoginController(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                return Ok(new { message = "Login successful" });
+            }
+            else
+            {
+                return Unauthorized(new { message = "Invalid username or password" });
+            }
+        }
+    }
+
+    public class LoginModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
+
